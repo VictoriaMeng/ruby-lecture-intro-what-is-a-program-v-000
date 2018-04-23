d2eb725de027707ed14253f03eb65524cffa1286
Victoria Meng <nights1ststar@protonmail.com>
2018-04-23 18:28:58 +0000

    automatically backed up by learn

--- 01_insert_cats_into_cats_table.sql
+++ 01_insert_cats_into_cats_table.sql
@@ -1,2 +1,1 @@
-INSERT INTO cats (name, age, breed) VALUES ('Lil\' Bub', 5, 'American Shorthair');
-INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
