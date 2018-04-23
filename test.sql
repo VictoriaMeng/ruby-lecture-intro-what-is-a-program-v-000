73d7a0196a46bb98bb46a4c0ee3a21135c81ecc3
Victoria Meng <nights1ststar@protonmail.com>
2018-04-23 19:07:38 +0000

    automatically backed up by learn

--- test.sql
+++ test.sql
@@ -3,10 +3,3 @@ INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1, "Tabby", 1);
 INSERT INTO cats (name, age, breed, owner_id) VALUES ("Lil' Bub", 5, "American Shorthair");
 INSERT INTO cats (name, age, breed, owner_id) VALUES ("Moe", 10, "Tabby");
 INSERT INTO cats (name, age, breed, owner_id) VALUES ("Patches", 2, "Calico");
-
-
-("Maru", 3 , "Scottish Fold", 1);
-("Hana", 1 , "Tabby", 1);
-("Lil\' Bub", 5, "American Shorthair");
-("Moe", 10, "Tabby");
-("Patches", 2, "Calico");
