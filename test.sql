64b8298715f263abcf4686ecbc2295ef33f8487c
Victoria Meng <nights1ststar@protonmail.com>
2018-04-23 19:00:52 +0000

    automatically backed up by learn

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
-INSERT INTO cats (name, age, breed, owner_id) VALUES 
+INSERT INTO cats (name, age, breed, owner_id) VALUES
 
 ("Maru", 3 , "Scottish Fold", 1);
-sqlite> INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1 , "Tabby", 1);
-sqlite> INSERT INTO cats (name, age, breed) VALUES ("Lil\' Bub", 5, "American Shorthair");
-sqlite> INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
-sqlite> INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
+("Hana", 1 , "Tabby", 1);
+("Lil\' Bub", 5, "American Shorthair");
+("Moe", 10, "Tabby");
+("Patches", 2, "Calico");
